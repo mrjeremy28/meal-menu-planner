fd618243c78a0059c1cb1c7c012f42cffc017806
Jeremy Adams <mr.jeremy.adams@gmail.com>
2024-03-29 05:40:26 -0700

    clean up db schema/migration to start over

--- db/migrate/20240318123658_create_join_table_tag_task.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableTagTask < ActiveRecord::Migration[7.1]
-  def change
-    create_join_table :tags, :tasks do |t|
-      t.index [:tag_id, :task_id]
-      t.index [:task_id, :tag_id]
-    end
-  end
-end
