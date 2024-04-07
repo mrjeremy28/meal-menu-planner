8924407b043c7608c5e2d39f5ca3b82e03529d8b
Jeremy Adams <mr.jeremy.adams@gmail.com>
2024-04-07 15:49:04 -0700

    add breadcrumbs to category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,15 +1,20 @@
 class CategoriesController < ApplicationController
+  before_action :set_breadcrumbs, only: [:show, :new, :edit, :delete]
 
   def index
     @categories = Category.order(:name)
+    add_breadcrumb("Home", "/")
+    add_breadcrumb("Categories")
   end
 
   def show
     @category = Category.find(params[:id])
+    add_breadcrumb(@category.name)
   end
 
   def new
     @category = Category.new
+    add_breadcrumb("New")
   end
 
   def create
@@ -23,6 +28,8 @@ class CategoriesController < ApplicationController
 
   def edit
     @category = Category.find(params[:id])
+    add_breadcrumb(@category.name, categories_path(@task))
+    add_breadcrumb("Edit")
   end
 
   def update
@@ -36,6 +43,7 @@ class CategoriesController < ApplicationController
 
   def delete
     @category = Category.find(params[:id])
+    add_breadcrumb("Delete")
   end
 
   def destroy
@@ -45,6 +53,11 @@ class CategoriesController < ApplicationController
   end
 
   private
+
+  def set_breadcrumbs
+    add_breadcrumb("Home", "/")
+    add_breadcrumb("Categories", categories_path)
+  end
 
   def category_params
     params.require(:category).permit(:name)
