f1ac0e2d9c5e7dac1e7bb5c4cc138cfee0be3894
Jeremy Adams <mr.jeremy.adams@gmail.com>
2024-04-07 13:48:33 -0700

    added button for jquery testing

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -2,6 +2,14 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
-    this.element.textContent = "Hello World!"
+    // this.element.textContent = "Hello World!"
+console.log("HELLO WORLD")
+    var initial_val = 0;
+    $("#btn-click").click(function (e) { 
+      e.preventDefault();
+      var date_value = $("#datepicker").val();
+      alert(`button has been clicked ${initial_val} and date ${date_value} `);
+      initial_val+= 1;
+    });
   }
 }
