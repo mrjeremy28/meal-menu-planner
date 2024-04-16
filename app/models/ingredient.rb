b11c4671541866f7314dfda22cb7af02eb31d890
Jeremy Adams <mr.jeremy.adams@gmail.com>
2024-04-16 11:33:31 -0700

    add name validation

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -4,4 +4,8 @@ class Ingredient < ApplicationRecord
   has_many :recipe_ingredients, dependent: :destroy
   has_many :recipes, through: :recipe_ingredients
   has_many :quantity_units, through: :recipe_ingredients
+
+  validates_presence_of :name
+  validates_length_of :name, maximum: 50
+
 end
